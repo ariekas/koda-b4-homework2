50ac3d8f09b4a16a20af296d21df37c1d5d304d6
ariekas <ariariekas@gmail.com>
2025-09-12 15:23:18 +0700

    feat: add array data new

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const { question, exiting } = require("./lib/question/index");
+const { question, exiting } = require("./lib/question/index")
 
 async function menuUtama() {
-  console.log("Selamat datang di Pizza Hut, Silahkan pilih menu: ");
-  console.log("1. Lihat Menu");
-  console.log("2. Lihat Keranjang");
-  console.log("3. Lihat History");
-  console.log("4. EXIT");
-  console.log("========================================");
+  console.log("Selamat datang di Pizza Hut, Silahkan pilih menu: ")
+  console.log("1. Lihat Menu")
+  console.log("2. Lihat Keranjang")
+  console.log("3. Lihat History")
+  console.log("4. EXIT")
+  console.log("========================================")
 
   const inputMenu = parseInt(
     await question("Masukan angka untuk memilih menu: ")
-  );
+  )
 
   switch (inputMenu) {
-    case 1:
-      console.log("Lihat menu");
-      exiting();
-      break;
-    case 2:
-      console.log("lihat keranjang");
-      exiting();
-      break;
-    case 3:
-      console.log("lihat history");
-      exiting();
-      break;
-    case 4:
-      console.log("exiting");
-      exiting();
-      break;
-    default:
-      console.log("menu tidak ada");
-      exiting();
-      break;
+  case 1:
+    console.log("Lihat menu")
+    exiting()
+    break
+  case 2:
+    console.log("lihat keranjang")
+    exiting()
+    break
+  case 3:
+    console.log("lihat history")
+    exiting()
+    break
+  case 4:
+    console.log("exiting")
+    exiting()
+    break
+  default:
+    console.log("menu tidak ada")
+    exiting()
+    break
   }
 }
 
-menuUtama();
+menuUtama()
